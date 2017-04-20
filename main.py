6c7b2aae23f1037790e3ec61172fe8bccf24bb9f
Nicholas Spadaro <spadaro.nicholas@gmail.com>
2017-04-20 08:43:31 +0200

    Add 2 more movie\n Little html modifications

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ star_wars4= media.Movie("Star Wars IV - A new hope",
                         "https://upload.wikimedia.org/wikipedia/en/8/87/StarWarsMoviePoster1977.jpg",
                         "https://www.youtube.com/watch?v=9gvqpFbRKtQ")
 movie_list.append(star_wars4)
+star_wars_rogue= media.Movie("Star Wars - Rogue One",
+                        "https://images-na.ssl-images-amazon.com/images/M/MV5BMjEwMzMxODIzOV5BMl5BanBnXkFtZTgwNzg3OTAzMDI@._V1_SX300.jpg",
+                        "https://www.youtube.com/watch?v=frdj1zb9sMY")
+movie_list.append(star_wars_rogue)
+hp_azkaban= media.Movie("Harry Potter and the Prisoner of Azkaban",
+                        "https://images-na.ssl-images-amazon.com/images/M/MV5BMTY4NTIwODg0N15BMl5BanBnXkFtZTcwOTc0MjEzMw@@._V1_SX300.jpg",
+                        "https://www.youtube.com/watch?v=lAxgztbYDbs")
+movie_list.append(hp_azkaban)
 toy_story = media.Movie('Toy story',
                         'https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg',
                         'https://www.youtube.com/watch?v=vwyZH85NQC4')
